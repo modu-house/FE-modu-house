79ad320de53487353ffff1dd53d2767f8a50019b
123456soobin-choi <sholong56@gmail.com>
2022-12-19 17:24:43 +0900

    💄 Add Login page Login 페이지 UI 추가, theme.js 수정(boxShadow 추가)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,68 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Input from '../components/common/Input';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <BgPaper>
+      <FormBlock>
+        <PageTitle>로그인</PageTitle>
+        <AuthForm>
+          <div className="inputBox">
+            <Input type="text" placeholder="아이디" />
+            <Input type="password" placeholder="패스워드" />
+          </div>
+          <button type="button">로그인</button>
+          <Link to="/register" className="register">
+            <span>회원가입</span>
+          </Link>
+        </AuthForm>
+      </FormBlock>
+    </BgPaper>
+  );
 }
 
 export default Login;
+
+const BgPaper = styled.div`
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.bg_paper2};
+`;
+
+const FormBlock = styled.div`
+  ${({ theme }) => theme.common.absoluteCenter};
+`;
+
+const PageTitle = styled.div`
+  text-align: center;
+  font-size: ${({ theme }) => theme.fontSizes.title};
+  font-weight: 500;
+  letter-spacing: 1px;
+  margin-bottom: 10px;
+`;
+
+const AuthForm = styled.div`
+  ${({ theme }) => theme.common.flexCenterColumn}
+  width: 600px;
+  height: 400px;
+  border-radius: 15px;
+  background-color: ${({ theme }) => theme.colors.bg_paper2};
+  ${({ theme }) => theme.common.boxShadow};
+
+  .inputBox {
+    ${({ theme }) => theme.common.flexCenterColumn}
+
+    input {
+      margin-bottom: 10px;
+    }
+  }
+
+  .register {
+    margin-top: 10px;
+  }
+
+  span {
+    color: ${({ theme }) => theme.colors.text3};
+  }
+`;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -9,6 +9,7 @@ const fontSizes = {
 const colors = {
   text1: '#000000',
   text2: '#D3D3D3',
+  text3: '#808080',
   primary1: '#FEA1A1',
   bg_paper1: '#F9FAFB',
   bg_paper2: '#FFFFFF',
@@ -32,6 +33,10 @@ const common = {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+  `,
+  boxShadow: `
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
   `,
 };
 
