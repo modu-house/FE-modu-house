ca2a1a75cc05cbcc18a90d6acfcee3507ae16f47
Jdentity <identityjang@gmail.com>
2022-12-19 17:09:32 +0900

    ♻️ Fix Input component
    Input 컴포넌트 리팩토링

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,64 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-function Input({ type, placeholder, onChange, value }) {
-  const [hasContent, setHasContent] = useState(false);
-
-  const handleFloat = (event) => {
-    if (event.target.value !== '') {
-      setHasContent(true);
-    } else {
-      setHasContent(false);
-    }
-  };
-
-  useEffect(() => {
-    if (value !== '') {
-      setHasContent(true);
-    } else {
-      setHasContent(false);
-    }
-  }, [value]);
-
+function Input({ type, placeholder, onChange, value, width }) {
   return (
-    <StyledInput>
-      <input
-        type={type}
-        placeholder={placeholder}
-        onFocus="this.placeholder = ''"
-        onBlur={handleFloat}
-        className={hasContent ? 'has-content' : ''}
-        onChange={onChange}
-        value={value}
-      />
-    </StyledInput>
+    <StyledInput
+      type={type}
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+      width={width}
+    />
   );
 }
 
 export default Input;
 
-const StyledInput = styled.div`
-  width: 100%;
-  margin: 0 auto;
-  position: relative;
+const StyledInput = styled.input`
+  width: ${(props) => props.width || '400px'};
+  height: 40px;
+  font-size: 18px;
+  text-indent: 10px;
+  border: 2px solid #d3d3d3;
+  border-radius: 10px;
 
-  input {
-    width: 400px;
-    height: 40px;
-    font-size: 18px;
-    text-indent: 10px;
-    border: 2px solid #d3d3d3;
-    border-radius: 10px;
-  }
-
-  input:focus,
-  input.has-content + ::placeholder {
+  &:focus {
     outline: none;
     border-color: ${({ theme }) => theme.colors.primary1};
-    color: ${({ theme }) => theme.colors.primary1};
-  }
-
-  input:focus::placeholder {
-    color: transparent;
   }
 `;
