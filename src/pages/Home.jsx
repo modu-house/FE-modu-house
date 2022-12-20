aaaea43bae51bc1f628161e7b8da8b91c7697c69
Jdentity <identityjang@gmail.com>
2022-12-20 19:25:25 +0900

    🤡 Add mock boards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import MainPageTemplate from '../components/main/MainPageTemplate';
-import axios from 'axios';
 
 function Home() {
-  // 테스트 코드
-  axios.get('/todos').then((res) => {
-    console.log(res.data);
-  });
   return <MainPageTemplate>Home</MainPageTemplate>;
 }
 
