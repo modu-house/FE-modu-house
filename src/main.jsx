2454b3f22903d9407ba9286bd1994d87c75fefb2
Jdentity <identityjang@gmail.com>
2022-12-20 16:02:20 +0900

    🤡 Add Mock Api setting

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
+import { worker } from './mocks/worker';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
